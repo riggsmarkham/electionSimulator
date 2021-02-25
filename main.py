e7d682e50ec6b1a30ae0ddd2a432ed1ad45c2d01
Riggs Markham <riggsmarkham@gmail.com>
2021-02-25 19:29:12 +0000

    changed to numpy

--- main.py
+++ main.py
@@ -1,79 +1,8 @@
-from numpy import random
-from math import sqrt
+import plurality as p
 
 NUM = 10000
 
-#normalize polling to decimals between 0 (none) and 1 (all)
-def normalizePolling(polling):
-  total = sum(polling)
-  newPolling = []
-  for x in polling:
-    newPolling.append(x/total)
-  return newPolling
-
-#calculate standard deviation from sample size
-def calcSTDEV(sample):
-  return 1/sqrt(sample)
-
-#generate randomized iteration
-def randomizedIteration(polling, stdev):
-  results = []
-  for x in polling:
-    num = random.normal(x,stdev)
-    while num < 0 or num > 1:
-      num = random.normal(x,stdev)
-    results.append(num)
-  newTotal = sum(results)
-  for i in range(len(results)):
-    results[i] /= newTotal
-  return results
-
-#figure out the winner of a plurality (FPTP) election from results
-def runPlurality(results):
-  maxIndex = -1;
-  maxValue = 0.0;
-  for i in range(len(results)):
-    if results[i] > maxValue:
-      maxIndex = i
-      maxValue = results[i]
-  return maxIndex
-
-#run num iterations of a pluraity election
-def runIterations(polling, num, sample):
-  stdev = calcSTDEV(sample)
-  normalized = normalizePolling(polling)
-  winList = [0 for i in range(len(polling))]
-  for i in range(num):
-    results = randomizedIteration(normalized, stdev)
-    winner = runPlurality(results)
-    winList[winner] += 1
-  return winList
-
-#print results of simulated elections
-def printResults(names, results):
-  nameLength = max(len(x) for x in names)
-  #numLength = max(len(str(x)) for x in results)
-  total = sum(results)
-  for i in range(len(results)):
-    # s = "{:<" + str(nameLength) + "} {:>" + str(numLength) + "n} {:.2%}"
-    # print(s.format(names[i],results[i],results[i]/total))
-    s = "{:<" + str(nameLength) + "} {:>6.1%}"
-    print(s.format(names[i],results[i]/total))
-  print()
-
-#read data from file to run simulation
-def readFromFile(filename, num):
-  file = open(filename, 'r')
-  n = int(file.readline())
-  for i in range(n):
-    names = file.readline().split(',')
-    names[-1] = names[-1][:-1]
-    polling = [int(x) for x in file.readline().split(',')]
-    sample = int(file.readline())
-    printResults(names, runIterations(polling, num, sample))
-  file.close()
-
 print("Color Parties Example")
-readFromFile('data/colors.txt', NUM)
+p.runElection(p.readFromFile('data/colors.txt'), NUM)
 print("NYC Mayoral Election")
-readFromFile('data/nycmayor.txt',NUM)
+p.runElection(p.readFromFile('data/nycmayor.txt'), NUM)
---
+++ plurality.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+#normalize polling to decimals between 0 (none) and 1 (all)
+def normalizePolling(polling):
+  return np.array(polling/np.sum(polling))
+
+#calculate standard deviation from sample size
+def calcSTDEV(sample):
+  return 1/np.sqrt(sample)
+
+#generate randomized iteration
+def randomizedIteration(polling, stdev):
+  results = []
+  for x in polling:
+    num = np.random.normal(x,stdev)
+    while num < 0 or num > 1:
+      num = np.random.normal(x,stdev)
+    results.append(num)
+  results = np.array(results)
+  return np.array(results/np.sum(results))
+
+#figure out the winner of a plurality (FPTP) election from results
+def runPlurality(results):
+  return np.argmax(results)
+
+#run num iterations of a pluraity election
+def runIterations(polling, num, sample):
+  stdev = calcSTDEV(sample)
+  normalized = normalizePolling(polling)
+  winList = np.zeros(len(polling))
+  for i in range(num):
+    results = randomizedIteration(normalized, stdev)
+    winner = runPlurality(results)
+    winList[winner] += 1
+  return winList
+
+def readFromFile(filename):
+  data = []
+  with open(filename) as infile:
+      lines = infile.readlines()
+      n = int(lines[0][0])
+      for i in range(n):
+        names = lines[3*i+1].split(',')
+        names[-1] = names[-1][:-1]
+        polls = list(map(int,lines[3*i+2][:-1].split(',')))
+        num = int(lines[3*i+3])
+        data.append([np.array(names), np.array(polls), num])
+  return data
+
+def printResults(names, results):
+  nameLength = len(max(names, key=len))
+  total = np.sum(results)
+  for i in range(len(results)):
+    s = "{:<" + str(nameLength) + "} {:>6.1%}"
+    print(s.format(names[i],int(results[i])/total))
+  print()
+
+def runElection(npData, num):
+  for el in npData:
+    printResults(el[0], runIterations(el[1], num, el[2]))
